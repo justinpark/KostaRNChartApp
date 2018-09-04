20624bd0269d06378ddc398471b8c4b5c1c8f709
Justin Park <justin.park@airbnb.com>
2018-09-03 20:48:42 -0700

    리스트 뷰

--- App.js
+++ App.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View, Dimensions} from 'react-native';
-import { Button, BottomNavigation } from 'react-native-material-ui';
+import {Platform, StyleSheet, Text, View, Dimensions, FlatList} from 'react-native';
+
+import { Button, Avatar, BottomNavigation, ListItem, Subheader } from 'react-native-material-ui';
 import {
   LineChart,
 } from 'react-native-chart-kit'
@@ -20,46 +21,126 @@ class App extends Component {
     super(props);
     this.state = {
       active: 'today',
+      chartData: [
+        Math.random() * 100,
+        Math.random() * 100,
+        Math.random() * 100,
+        Math.random() * 100,
+        Math.random() * 100,
+        Math.random() * 100
+      ],
     };
+    this.layout = [{
+      id: 'layout01',
+      type: 'title',
+    }, {
+      id: 'layout02',
+      type: 'chart',
+    }, {
+      id: 'layout03',
+      type: 'button',
+    }, {
+      id: 'layout04',
+      type: 'intro',
+    }, {
+      id: 'layout05',
+      type: 'intro',
+    }, {
+      id: 'layout06',
+      type: 'intro',
+    }, {
+      id: 'layout07',
+      type: 'intro',
+    }, {
+      id: 'layout08',
+      type: 'intro',
+    }, {
+      id: 'layout09',
+      type: 'intro',
+    }, {
+      id: 'layout10',
+      type: 'intro',
+    }, {
+      id: 'layout11',
+      type: 'intro',
+    }];
   }
+  renderItem({ item }) {
+    if(item.type === 'title') {
+      return (
+        <Text style={styles.welcome}>글로벌 SW 강좌</Text>
+      );
+    }
+
+    if (item.type === 'chart') {
+      return (
+        <LineChart
+          data={{
+            labels: ['January', 'February', 'March', 'April', 'May', 'June'],
+            datasets: [{
+              data: [
+                Math.random() * 100,
+                Math.random() * 100,
+                Math.random() * 100,
+                Math.random() * 100,
+                Math.random() * 100,
+                Math.random() * 100
+              ]
+            }]
+          }}
+          width={Dimensions.get('window').width - 10} // from react-native
+          height={Dimensions.get('window').height / 2}
+          chartConfig={{
+            backgroundColor: '#03a9f4',
+            backgroundGradientFrom: '#03a9f4',
+            backgroundGradientTo: '#03a9f4',
+            color: (opacity = 1) => `rgba(255, 255, 255, ${opacity})`,
+            style: {
+              borderRadius: 16
+            }
+          }}
+          bezier
+          style={{
+            marginVertical: 8,
+            borderRadius: 16
+          }}
+        />
+      );
+    }
+
+    if (item.type === 'button') {
+      return (
+        <Subheader text="매매 현황" />
+      );
+    }
+
+    if (item.type === 'intro') {
+      return (
+        <ListItem
+          divider
+          leftElement={<Avatar text="BCC" />}
+          centerElement={{
+            primaryText: '비트코인 (BCC/BTC)',
+            secondaryText: `${Math.random()}`,
+          }}
+          rightElement={<Button raised primary text="구매" />}
+          onPress={() => {}}
+        />
+      );
+    }
+
+    return null;
+  }
+
   render() {
     return (
       <Container>
         <View style={styles.contentContainer}>
-          <Text style={styles.welcome}>글로벌 SW 강좌</Text>
-          <LineChart
-            data={{
-              labels: ['January', 'February', 'March', 'April', 'May', 'June'],
-              datasets: [{
-                data: [
-                  Math.random() * 100,
-                  Math.random() * 100,
-                  Math.random() * 100,
-                  Math.random() * 100,
-                  Math.random() * 100,
-                  Math.random() * 100
-                ]
-              }]
-            }}
-            width={Dimensions.get('window').width - 10} // from react-native
-            height={Dimensions.get('window').height / 2}
-            chartConfig={{
-              backgroundColor: '#03a9f4',
-              backgroundGradientFrom: '#03a9f4',
-              backgroundGradientTo: '#03a9f4',
-              color: (opacity = 1) => `rgba(255, 255, 255, ${opacity})`,
-              style: {
-                borderRadius: 16
-              }
-            }}
-            bezier
-            style={{
-              marginVertical: 8,
-              borderRadius: 16
-            }}
+          <FlatList
+            data={this.layout}
+            keyExtractor={(item) => item.id}
+            renderItem={this.renderItem}
           />
-          <Button raised primary text="Primary" />
-          <Text style={styles.instructions}>{instructions}</Text>
         </View>
         <BottomNavigation active={this.state.active} hidden={false} >
           <BottomNavigation.Action
@@ -99,7 +180,7 @@ const styles = StyleSheet.create({
     alignItems: 'center',
   },
   instructions: {
-    textAlign: 'center',
+    textAlign: 'left',
     color: '#333333',
     marginBottom: 5,
   },
