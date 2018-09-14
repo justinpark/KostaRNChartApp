e25f830e634db8fd78aaa77ff87f081f3f9b67d5
Justin Park <justin.park@airbnb.com>
2018-09-14 00:23:01 -0700

    devtools 플러그인 제거

--- js/configureStore.js
+++ js/configureStore.js
@@ -2,7 +2,7 @@ import { createStore, combineReducers, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
 import { middleware as reduxPackMiddleware } from 'redux-pack'
 import logger from 'redux-logger'
-import { composeWithDevTools } from 'redux-devtools-extension';
+// import { composeWithDevTools } from 'redux-devtools-extension';
 
 import reducers from './reducers';
 import createReducers from './api-redux-pack/createReducers';
@@ -11,13 +11,13 @@ import hideToastMessageEffect from './middlewares/hideToastMessageEffect';
 
 const apiReducers = createReducers('transactions', 'coins', 'users');
 
-const finalCreateStore = composeWithDevTools(applyMiddleware(
+const finalCreateStore = applyMiddleware(
   thunk,
   reduxPackMiddleware,
   hideToastMessageEffect(1000),
   // setFilterEffect,
   logger,
-))(createStore);
+)(createStore);
 
 export default (initState) => {
   return finalCreateStore(combineReducers({ ...reducers, ...apiReducers }), initState);
